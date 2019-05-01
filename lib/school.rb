c35e29caa79e6d5e25742297b8142305d94bb556
Oscar Shoudt <shoudto@gmail.com>
2019-05-01 17:23:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  
+  attr_reader :add_student, :grade, :sort 
+  
+  def initialize
+    
+    new = []
+    
+  end 
+  
+  def add_student=(student)
+    
+    @student = student
+    
+  end 
+  
