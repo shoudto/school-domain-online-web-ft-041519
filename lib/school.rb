ea2479f8fb1591484551939933c10a0d8e149fbf
Oscar Shoudt <shoudto@gmail.com>
2019-05-02 19:23:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,31 @@
-class School
+require 'pry'
+class School 
   
-  attr_reader :add_student, :grade, :sort 
+  attr_accessor :name, :roster 
   
-  def initialize
-    
-    new = []
-    
+  def initialize(name)
+    @name = name 
+    @roster = {}
   end 
   
-  def add_student=(student)
+  def add_student(student_name, grade)
     
-    @student = student
-    
-  end 
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    # @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] << student_name
+  end
   
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    sorted_roster = {}
+    @roster.each do |grade, students|
+      sorted_roster[grade] = students.sort 
+    end 
+    sorted_roster
+  end 
+end 
